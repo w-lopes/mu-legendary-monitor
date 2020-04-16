c5d9ed770d77a1caeb5bac708e964187dfe1af83
Wellington Lopes <w.camara.lopes@gmail.com>
2020-04-16 13:45:21 -0300

    voltei refresh

--- index.php
+++ index.php
@@ -143,7 +143,7 @@
                     M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
 
                     var update_time = Math.floor(Math.random() * (30000 - 10000 + 1)) + 10000;
-                    // window.setTimeout(loadLevels, update_time);
+                    window.setTimeout(loadLevels, update_time);
                 });
             })();
         });
